a112a88f9329a6d06258d40205ba5ce5a4c73cc0
Michael-Rullo <michaelrullo2006@gmail.com>
2024-12-04 23:11:30 -0500

    update

--- adhd_chatbot/src/chatbot.py
+++ adhd_chatbot/src/chatbot.py
@@ -6,7 +6,7 @@ class Api:
         
         self.client = OpenAI(api_key= api_key)
         self.client.files.create(
-        file=open(r"adhd_chatbot\ADHD_training_set.jsonl", "rb"),
+        file=open(r"adhd_chatbot/ADHD_training_set.jsonl", "rb"),
         purpose= "fine-tune"
         )
     
