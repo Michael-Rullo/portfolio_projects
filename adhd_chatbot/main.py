798f3bc16e2d724a2576ec98c5a018d45bc33e5a
Michael-Rullo <michaelrullo2006@gmail.com>
2024-12-04 23:53:29 -0500

    update

--- adhd_chatbot/main.py
+++ adhd_chatbot/main.py
@@ -1,10 +1,8 @@
 from src.controller import Controller
 
-
 def main():
     controller = Controller()
     controller.mainloop()
-    
 
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
